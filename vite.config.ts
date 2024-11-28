9ecab42df163924709c20148cb11199de2001de8
Rami Nakkar <rami.nakkar00@gmail.com>
2024-11-28 18:09:07 +0100

    add scss

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  },
   base: "/",
   plugins: [react()],
   preview: {
