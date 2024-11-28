3efd84e039045c14a25ff338d3139c5b9f96a2a5
Rami Nakkar <rami.nakkar00@gmail.com>
2024-11-28 19:05:30 +0100

    add chakra ui deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
 import App from './App.tsx'
+import {ChakraProvider, defaultSystem} from '@chakra-ui/react'
+import { ThemeProvider } from 'next-themes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <ChakraProvider value={defaultSystem}>
+          <ThemeProvider attribute="class" disableTransitionOnChange>
+              <App />
+          </ThemeProvider>
+      </ChakraProvider>
   </StrictMode>,
 )
