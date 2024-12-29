9c2163e48c90ca22a3ffb61bae5a9105b643bd48
Lucperrom <luciaperezromero07@gmail.com>
2024-12-29 13:39:32 +0100

    test: Test de selenium y mejora de Frontend #20
    Co-authored-by: danmoltor2 <danmoltor2@users.noreply.github.com>

--- src/services/rating/RatingList.tsx
+++ src/services/rating/RatingList.tsx
@@ -191,34 +191,29 @@ function RatingList() {
                     </div>
                   </div>
                   {/* {userId!="" && userId==rating.id &&( */}
-                  <div className="rating-options">
-                  <button onClick={() => {setIsPopoverOpen(true); setRatingId(rating.id)}} className="edit-button" style={{ textDecoration: "none" }}
-                  ><i className="fas fa-edit"></i>Edit Review</button>
-                  <button
-                  onClick={() => handleGenerateText(rating.description)}
-                  className="linkedIn"
-                  style={{ display: "flex", alignItems: "center", gap: "8px" }}
-                >
-                  <svg
-                    className="share-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    style={{ width: 15 }}
-                    viewBox="0 0 512 512"
-                    aria-label="ln"
-                    role="img"
-                  >
-                    <path d="m132.28,479.99501l-92.88,0l0,-299.1l92.88,0l0,299.1zm-46.49,-339.9c-29.7,0 -53.79,-24.6 -53.79,-54.3a53.79,53.79 0 0 1 107.58,0c0,29.7 -24.1,54.3 -53.79,54.3zm394.11,339.9l-92.68,0l0,-145.6c0,-34.7 -0.7,-79.2 -48.29,-79.2c-48.29,0 -55.69,37.7 -55.69,76.7l0,148.1l-92.78,0l0,-299.1l89.08,0l0,40.8l1.3,0c12.4,-23.5 42.69,-48.3 87.88,-48.3c94,0 111.28,61.9 111.28,142.3l0,164.3l-0.1,0z"></path>
-                  </svg>
-                  Share on LinkedIn
-                </button>
-                  <button
-                    onClick={() => removeRating(rating.id)}
-                    className="danger-button"
-                    style={{ display: "flex", alignItems: "center", gap: "8px" }}
-                    ><i className="fa fa-trash" aria-hidden="true"></i> Delete
-                  </button>
-
-                  </div>
+                    <div className="rating-options">
+                      <button 
+                        onClick={() => {setIsPopoverOpen(true); setRatingId(rating.id)}} 
+                        className="edit-button" 
+                        style={{ alignItems: "center", gap: "8px" }}
+                        ><i className="fas fa-edit" style={{ marginRight: "8px" }}></i> 
+                        Edit
+                      </button>
+                      <button
+                        onClick={() => handleGenerateText(rating.description)}
+                        className="linkedIn"
+                        style={{ alignItems: "center", gap: "8px" }}
+                        ><i className="fab fa-linkedin-in" style={{ marginRight: "8px" }}></i> 
+                        Share on LinkedIn
+                      </button>
+                      <button
+                        onClick={() => removeRating(rating.id)}
+                        className="danger-button"
+                        style={{ alignItems: "center", gap: "8px" }}
+                        ><i className="fa fa-trash" aria-hidden="true" style={{ marginRight: "8px" }}></i> 
+                        Delete
+                      </button>
+                    </div>
               {/* )} */}
 
                 </div>
