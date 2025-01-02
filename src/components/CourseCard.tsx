3ab21ec136b03aed67224910eec17729d158600b
Jairogonza7 <jairomega19@gmail.com>
2025-01-02 20:25:43 +0100

    Fix: improve layout and styling of CourseCard component

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -53,37 +53,36 @@ const CourseCard: React.FC<CourseCardProps> = ({ id, name, description, category
             <Card.Body >
             <Card.Title mb="2"> {name}</Card.Title>
                 <Card.Description mb={4}> {description} </Card.Description>
-                <HStack align="center" gap={4}>
+                <HStack align="center" gap={4} flexWrap="wrap">
                    <Badge colorScheme="teal">{category}</Badge>
                     <Badge colorScheme="blue">{duration}</Badge>
                     <Badge colorScheme="purple">{level}</Badge>
                     
                 </HStack>
             </Card.Body>
-           
             <Card.Footer>
-            <Flex justify="space-between" align="center" width="100%">
-          <HStack>
-            <LinkButton colorScheme="teal" variant="solid" to={`/courses/${id}`}>
-              Enter <IoEnterOutline />
-            </LinkButton>
-            <IconButton aria-label="Delete course" onClick={handleDelete} colorScheme="red">
-              <Trash2 />
-            </IconButton>
-            <IconButton aria-label="Update course" onClick={handleUpdate} >
-            <SquarePen />
-            </IconButton>
-          </HStack>
-          <Box> 
+            <Flex justify="space-between" align="center" width="100%" flexDirection={{ base: 'column', md: 'row' }}>
+            <Box>
+            <HStack gap={4} mb={{ base: 4, lg: 4 }} flexWrap="wrap">
+              <LinkButton colorScheme="teal" variant="solid" to={`/courses/${id}`}>
+                Enter <IoEnterOutline />
+              </LinkButton>
+              <IconButton aria-label="Delete course" onClick={handleDelete} colorScheme="red">
+                <Trash2 />
+              </IconButton>
+              <IconButton aria-label="Update course" onClick={handleUpdate}>
+                <SquarePen />
+              </IconButton>
+            </HStack>
+            <HStack mt={4} gap={4} mb={{ base: 4, md: 0 }} flexWrap="wrap">
             <LinkButton to={`/courses/${id}/ratings`} fontSize="sm" color="teal.500">
               View all ratings
             </LinkButton>
             <Badge fontSize="2xl" fontWeight="bold">
-              Rating: {rating || 0}  <FaStar color="gold"/>
+              Rating: {rating || 0} <FaStar color="gold" />
             </Badge>
-            
+            </HStack>
           </Box>
-          
         </Flex>
             </Card.Footer>
         </Card.Root>
